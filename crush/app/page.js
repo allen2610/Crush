cf0f10713e2aa0bd5501b24b18f168ff6c445611
allen2610 <jessicaallen.lim@gmail.com>
2024-06-20 18:26:30 +0700

    add: video and change

--- crush/app/page.js
+++ crush/app/page.js
@@ -1,21 +1,38 @@
+"use client"
 import Image from "next/image";
-import shy from "@/public/shyduck.jpeg"
+import React from "react";
+import {useState, useEffect} from 'react';
+
 
 export default function Home() {
-  return (
+    const [Yes,setYes] = useState(false)
+  
+    return (
     <main className="bg-pink-100 flex relative h-screen flex-col items-center justify-center p-32">
       <h1 className="w-full text-center text-pink-500 font-bold text-3xl">
-        Will you go out with me?
+        {Yes ? "Maaciii ehehe, see u on the 22nd!" : "Will you have a date with me?"}
       </h1>
+      <video
+          autoPlay
+          muted
+          loop
+          className={`m-10  ${Yes ? "hidden opacity-0" : "opacity-100"}`}
+          width = {280}
+        >
+          <source src="/shy0.webm" type="video/webm"/>
+      </video>
+      <video
+          autoPlay
+          muted
+          loop
+          className={`m-10 ${Yes ? "opacity-100" : "hidden opacity-0"}`}
+          width = {280}
+        >
+          <source src="/shy1.webm" type="video/webm"/>
+      </video>
+
       <div>
-        <Image className="m-10"
-          src={shy}
-          width={250}
-        />
-       
-      </div>
-      <div>
-        <button className="bg-pink-500 rounded-3xl px-12 py-1 text-white m-10">
+        <button onClick={() => setYes(true)} className="bg-pink-500 rounded-3xl px-12 py-1 text-white m-10">
           Yes
         </button>
         <button className="bg-pink-100 rounded-3xl px-12 py-1 text-pink-500 outline m-10">
@@ -24,5 +41,5 @@ export default function Home() {
       </div>
         
     </main>
-  );
-}
+    );
+  }
